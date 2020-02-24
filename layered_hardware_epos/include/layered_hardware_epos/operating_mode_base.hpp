5508f43f3d76313ab8688cd1394c2617cc91de4a
Yoshito Okada <okada@rm.is.tohoku.ac.jp>
2020-02-24 15:34:27 +0900

    remove lines no longer used

--- layered_hardware_epos/include/layered_hardware_epos/operating_mode_base.hpp
+++ layered_hardware_epos/include/layered_hardware_epos/operating_mode_base.hpp
@@ -1,19 +1,12 @@
 #ifndef LAYERED_HARDWARE_EPOS_OPERATING_MODE_BASE_HPP
 #define LAYERED_HARDWARE_EPOS_OPERATING_MODE_BASE_HPP
 
-#include <cmath>
-#include <limits>
-#include <map>
 #include <string>
 
 #include <layered_hardware_epos/epos_actuator_data.hpp>
-#include <ros/console.h>
 #include <ros/duration.h>
 #include <ros/time.h>
 
-#include <boost/cstdint.hpp>
-#include <boost/foreach.hpp>
-#include <boost/math/special_functions/fpclassify.hpp> // for isnan()
 #include <boost/shared_ptr.hpp>
 
 namespace layered_hardware_epos {
@@ -38,17 +31,6 @@ public:
 
 protected:
   // TODO: implement common operations ??
-
-  //
-  // utility
-  //
-
-  static bool isNotNaN(const double a) { return !boost::math::isnan(a); }
-
-  static bool areNotEqual(const double a, const double b) {
-    // does !(|a - b| < EPS) instead of (|a - b| >= EPS) to return True when a and/or b is NaN
-    return !(std::abs(a - b) < std::numeric_limits< double >::epsilon());
-  }
 
 protected:
   const std::string name_;
