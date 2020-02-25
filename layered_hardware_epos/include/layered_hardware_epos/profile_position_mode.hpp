8e34f54495c1572a59aac97862253fef2c9cb6fc
Yoshito Okada <okada@rm.is.tohoku.ac.jp>
2020-02-25 15:18:34 +0900

    add profile {position, velocity} modes

---
+++ layered_hardware_epos/include/layered_hardware_epos/profile_position_mode.hpp
@@ -0,0 +1,96 @@
+#ifndef LAYERED_HARDWARE_EPOS_PROFILE_POSITION_MODE_HPP
+#define LAYERED_HARDWARE_EPOS_PROFILE_POSITION_MODE_HPP
+
+#include <limits>
+
+#include <epos_command_library_cpp/exception.hpp>
+#include <layered_hardware_epos/common_namespaces.hpp>
+#include <layered_hardware_epos/epos_actuator_data.hpp>
+#include <layered_hardware_epos/operating_mode_base.hpp>
+#include <ros/console.h>
+#include <ros/duration.h>
+#include <ros/time.h>
+
+#include <boost/math/special_functions/fpclassify.hpp> // for isnan()
+
+namespace layered_hardware_epos {
+
+class ProfilePositionMode : public OperatingModeBase {
+public:
+  ProfilePositionMode(const EposActuatorDataPtr &data)
+      : OperatingModeBase("profile_position", data) {}
+
+  virtual void starting() {
+    try {
+      // switch to position mode
+      *data_->node.setEnableState();
+      *data_->node.activateProfilePositionMode();
+
+      // initialize position command using present position
+      data_->pos_cmd = *data_->node.getPosition(data_->count_per_revolution);
+      prev_pos_cmd_ = std::numeric_limits< double >::quiet_NaN();
+
+      // initialize profile velocity command using present value
+      *data_->node.getPositionProfile(&data_->vel_cmd, &prof_acc_, &prof_dec_);
+      prev_vel_cmd_ = data_->vel_cmd;
+
+      has_started_ = true;
+    } catch (const eclc::Exception &error) {
+      ROS_ERROR_STREAM("ProfilePositionMode::starting(): " << error.what());
+      has_started_ = false;
+    }
+  }
+
+  virtual void read(const ros::Time &time, const ros::Duration &period) {
+    if (!has_started_) {
+      return;
+    }
+
+    try {
+      // read actuator states
+      data_->pos = *data_->node.getPosition(data_->count_per_revolution);
+      data_->vel = *data_->node.getVelocity();
+      data_->eff = *data_->node.getTorque(data_->torque_constant);
+    } catch (const eclc::Exception &error) {
+      ROS_ERROR_STREAM("ProfilePositionMode::read(): " << error.what());
+    }
+  }
+
+  virtual void write(const ros::Time &time, const ros::Duration &period) {
+    if (!has_started_) {
+      return;
+    }
+
+    try {
+      // write profile velocity if command has been updated
+      if (!boost::math::isnan(data_->vel_cmd) && data_->vel_cmd != prev_vel_cmd_) {
+        *data_->node.setPositionProfile(data_->vel_cmd, prof_acc_, prof_dec_);
+        prev_vel_cmd_ = data_->vel_cmd;
+      }
+
+      // write position command if updated
+      if (!boost::math::isnan(data_->pos_cmd) && data_->pos_cmd != prev_pos_cmd_) {
+        *data_->node.moveToPosition(data_->pos_cmd, data_->count_per_revolution,
+                                    /* absolute = */ true, /* immediately = */ true);
+        prev_pos_cmd_ = data_->pos_cmd;
+      }
+    } catch (const eclc::Exception &error) {
+      ROS_ERROR_STREAM("ProfilePositionMode::write(): " << error.what());
+    }
+  }
+
+  virtual void stopping() {
+    try {
+      *data_->node.setDisableState();
+    } catch (const eclc::Exception &error) {
+      ROS_ERROR_STREAM("ProfilePositionMode::stopping(): " << error.what());
+    }
+  }
+
+private:
+  bool has_started_;
+  double prev_pos_cmd_, prev_vel_cmd_, prof_acc_, prof_dec_;
+};
+} // namespace layered_hardware_epos
+
+#endif
---
+++ layered_hardware_epos/include/layered_hardware_epos/profile_velocity_mode.hpp
@@ -0,0 +1,83 @@
+#ifndef LAYERED_HARDWARE_EPOS_PROFILE_VELOCITY_MODE_HPP
+#define LAYERED_HARDWARE_EPOS_PROFILE_VELOCITY_MODE_HPP
+
+#include <limits>
+
+#include <epos_command_library_cpp/exception.hpp>
+#include <layered_hardware_epos/common_namespaces.hpp>
+#include <layered_hardware_epos/epos_actuator_data.hpp>
+#include <layered_hardware_epos/operating_mode_base.hpp>
+#include <ros/console.h>
+#include <ros/duration.h>
+#include <ros/time.h>
+
+#include <boost/math/special_functions/fpclassify.hpp> // for isnan()
+
+namespace layered_hardware_epos {
+
+class ProfileVelocityMode : public OperatingModeBase {
+public:
+  ProfileVelocityMode(const EposActuatorDataPtr &data)
+      : OperatingModeBase("profile_velocity", data) {}
+
+  virtual void starting() {
+    try {
+      // switch to velocity mode
+      *data_->node.setEnableState();
+      *data_->node.activateProfileVelocityMode();
+
+      // set reasonable initial command
+      data_->vel_cmd = 0.;
+      prev_vel_cmd_ = std::numeric_limits< double >::quiet_NaN();
+
+      has_started_ = true;
+    } catch (const eclc::Exception &error) {
+      ROS_ERROR_STREAM("ProfileVelocityMode::starting(): " << error.what());
+      has_started_ = false;
+    }
+  }
+
+  virtual void read(const ros::Time &time, const ros::Duration &period) {
+    if (!has_started_) {
+      return;
+    }
+
+    try {
+      data_->pos = *data_->node.getPosition(data_->count_per_revolution);
+      data_->vel = *data_->node.getVelocity();
+      data_->eff = *data_->node.getTorque(data_->torque_constant);
+    } catch (const eclc::Exception &error) {
+      ROS_ERROR_STREAM("ProfileVelocityMode::read(): " << error.what());
+    }
+  }
+
+  virtual void write(const ros::Time &time, const ros::Duration &period) {
+    if (!has_started_) {
+      return;
+    }
+
+    try {
+      if (!boost::math::isnan(data_->vel_cmd) && data_->vel_cmd != prev_vel_cmd_) {
+        *data_->node.setVelocityMust(data_->vel_cmd);
+        prev_vel_cmd_ = data_->vel_cmd;
+      }
+    } catch (const eclc::Exception &error) {
+      ROS_ERROR_STREAM("ProfileVelocityMode::write(): " << error.what());
+    }
+  }
+
+  virtual void stopping() {
+    try {
+      *data_->node.setDisableState();
+    } catch (const eclc::Exception &error) {
+      ROS_ERROR_STREAM("ProfileVelocityMode::stopping(): " << error.what());
+    }
+  }
+
+private:
+  bool has_started_;
+  double prev_vel_cmd_;
+};
+} // namespace layered_hardware_epos
+
+#endif
